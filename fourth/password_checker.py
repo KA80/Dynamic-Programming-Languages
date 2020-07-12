c376ab0ad4bcb76651b7bbedecc185a1a97a1ae4
Stanislav Nagornov <58802975+KA80@users.noreply.github.com>
2020-07-12 22:58:21 +1000

    Update password_checker.py

--- fourth/password_checker.py
+++ fourth/password_checker.py
@@ -1,7 +1,7 @@
 def password_level(pas):
     if len(pas) < 6:
         message = "Недопустимый пароль"
-    elif pas.isdigit() or pas.isalpha and (pas.islower() or pas.isupper()):
+    elif pas.isdigit() or pas.isalpha() and (pas.islower() or pas.isupper()):
         message = "Ненадежный пароль"
     elif pas.isalpha() and not (pas.islower() or pas.isupper()) or pas.isalnum() and (pas.islower() or pas.isupper()):
         message = "Слабый пароль"
